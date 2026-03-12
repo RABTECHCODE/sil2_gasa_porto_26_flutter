f43eee3903684632b88155214ad46a9014eccadd
RABTECHCODE <developer.rabtech@gmail.com>
2026-03-12 16:31:24 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/page_2.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+Widget myApp() {
+  return MaterialApp(title: "Notre App", home: Page2());
+}
